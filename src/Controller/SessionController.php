58ff4c255a177317288a597cf48a720fea31c929
raul.zannoni <raul.zannoni92@gmail.com>
2023-07-21 10:35:51 +0200

    bootstrap + tableau sessions

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Session;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +12,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class SessionController extends AbstractController
 {
     #[Route('/session', name: 'app_session')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $sessions = $entityManager->getRepository(Session::class)->findAll();
         return $this->render('session/index.html.twig', [
-            'controller_name' => 'SessionController',
+            'sessions' => $sessions
         ]);
     }
 }
