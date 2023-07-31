69ff7d3914e40a341feebe1e3efeaf672e5a2a8e
raul.zannoni <raul.zannoni92@gmail.com>
2023-07-31 09:10:42 +0200

    categorie: form->add/delete/edit

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -3,10 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Categorie;
+use App\Form\CategorieType;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CategorieController extends AbstractController
@@ -19,6 +22,68 @@ class CategorieController extends AbstractController
             'categories' => $categories
         ]);
     }
+
+    #[Route('/categorie/new', name: 'new_categorie')]
+    #[Route('/categorie/{id}/edit', name: 'edit_categorie')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    public function new_edit(Categorie $categorie = null, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        if(!$this->getUser()->isVerified()){
+            return $this->redirectToRoute('app_home');
+        }
+        if (!$this->isGranted("ROLE_ADMIN")) {
+            return $this->redirectToRoute('app_categorie');
+        } else {
+            if(!$categorie){
+                $categorie = new Categorie();
+            }
+    
+            $form = $this->createForm(CategorieType::class, $categorie);
+    
+            $form->handleRequest($request);
+    
+            if($form->isSubmitted() && $form->isValid()) {
+    
+                $session = $form->getData();
+                //prepare PDO
+                $entityManager->persist($session);
+                //execute PDO
+                $entityManager->flush();
+    
+                return $this->redirectToRoute('app_categorie');
+    
+            }
+            return $this->render('categorie/new.html.twig', [
+                'formAddCategorie' => $form,
+                'edit' => $categorie->getId()
+            ]);
+        }
+    }
+
+    #[Route('/categorie/{id}/remove', name: 'remove_categorie')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    public function remove(Categorie $categorie, EntityManagerInterface $entityManager): Response
+    {
+        if(!$this->getUser()->isVerified()){
+            return $this->redirectToRoute('app_home');
+        }
+        if($this->isGranted("ROLE_ADMIN")){
+            $entityManager->remove($categorie);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_categorie');
+    }
+
+
+
+
+
+
+
+
+
+
 
     #[Route('/categorie/{id}', name: 'show_categorie')]
     public function show(Categorie $categorie): Response
