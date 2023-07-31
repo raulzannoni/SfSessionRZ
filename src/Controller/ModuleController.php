266d1de3fefedaec02f205e79b7989deeec9be1d
raul.zannoni <raul.zannoni92@gmail.com>
2023-07-31 16:57:02 +0200

    check module program

--- src/Controller/ModuleController.php
+++ src/Controller/ModuleController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Module;
 use App\Entity\Session;
 use App\Form\ModuleType;
+use App\Entity\Represent;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,6 +13,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 class ModuleController extends AbstractController
 {
@@ -77,6 +79,60 @@ class ModuleController extends AbstractController
 
         return $this->redirectToRoute('app_module');
     }
+
+    /*  This method is developed to program the current module with a specified session (to add and remove)*/
+    #[Route('/module/{idModule}/program/{idSession}', name: 'program_module')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    #[ParamConverter("module", options: ['mapping' => ["idModule" => "id"]])]
+    #[ParamConverter("module", options: ['mapping' => ["idSession" => "id"]])]
+    public function program(Session $session, Module $module, EntityManagerInterface $entityManager, Request $request) {
+        
+        if(!$this->getUser()->isVerified()){
+            return $this->redirectToRoute('app_home');
+        }
+        if($this->isGranted("ROLE_ADMIN")){
+
+            $sessionsNotProgrammed = $entityManager->getRepository(Module::class)->findSessionsNotProgrammed($module->getId());
+            
+            if(in_array($session, $sessionsNotProgrammed)){
+                
+                $days = $request->get('days'); 
+                
+                $represent = new Represent();
+                $represent->setDays($days);
+                $session->addRepresent($represent);
+                $module->addRepresent($represent);
+                $entityManager->persist($represent);
+                $entityManager->flush();
+                
+            } else{
+                $representSession = iterator_to_array($session->getRepresents());
+                
+                $representModule = iterator_to_array($module->getRepresents());
+
+                $represent = new Represent();
+
+                foreach($representModule as $representM){
+                    foreach($representSession as $representS ) {
+                        if($representS == $representM){
+                            $represent = $representS;
+                            break;
+                        }
+                    }
+                }
+                //dd($represent);
+                $entityManager->remove($represent);
+                $entityManager->flush();
+            }
+            
+            $entityManager->persist($session);
+            $entityManager->persist($module);
+
+            $entityManager->flush();
+        }
+
+    }
+
 
     #[Route('/module/{id}', name: 'show_module')]
     #[IsGranted('IS_AUTHENTICATED_FULLY')]
